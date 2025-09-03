9ca52c7ace60caf8bd8fe05378915624e0f73fb9
KevinPalate <kevin2005kdp@gmail.com>
2025-09-03 13:04:23 -0500

    Agregegado DTO createOrderDetailDto

--- Northwind.Sales.Entities/Dtos/CreateOrder/CreateOrderDetailDto.cs
+++ Northwind.Sales.Entities/Dtos/CreateOrder/CreateOrderDetailDto.cs
@@ -1,5 +1,20 @@
 ﻿namespace Northwind.Sales.Entities.Dtos.CreateOrder;
 
-public class CreateOrderDetailDto
+//Primary Constructors c# 12
+public class CreateOrderDetailDto(int productId, decimal unitPrice, short quantity)
 {
+    public int ProductId => productId;  
+    public decimal UnitPrice => unitPrice;
+    public short Quantity => quantity;
+}
+
+public class CreateOrderDetailDto2
+{
+    private int productId;
+    private decimal unitPrice;
+    private short quantity;
+
+    public int ProductId { get { return productId; } }
+    public decimal UnitPrice { get { return unitPrice; } }
+    public short Quantity { get { return quantity; } }
 }
